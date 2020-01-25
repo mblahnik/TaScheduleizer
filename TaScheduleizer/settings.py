2ed7a7f05aaf1a243cf972d0dd8b50fd0c8985bc
mblahnik <mblahnik@uwm.edu>
2020-01-24 18:54:40 -0600

    Wasting time

--- TaScheduleizer/settings.py
+++ TaScheduleizer/settings.py
@@ -123,9 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'templates/StaticFiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'templates/StaticFiles'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'templates/StaticFiles'),
+#)
 
 GOOGLE_RECAPTCHA_SECRET_KEY = '6LdkVqEUAAAAAIISSRY-4GLXbSA4kaXzdyF3TzNm'
